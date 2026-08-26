b4d83b7d3a99412cbc1f91312b05caec53a943a3
stolas <eddyninko@gmail.com>
2026-08-26 01:18:00 +0100

    Correction in router folder of user.routes and add of the middleware require

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const userController = require("../controllers/user.controller");
+const userController = require("../controllers/user.controller.js");
+const {verifyToken, isAdmin} = require('../middlewares/authMiddleware');
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
