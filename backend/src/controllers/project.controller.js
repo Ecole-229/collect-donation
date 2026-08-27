d2fca086ca2554fa3aafdd62311027148fc9c587
stolas <eddyninko@gmail.com>
2026-08-27 03:25:51 +0100

    Project creation function done

---
+++ backend/src/controllers/project.controller.js
@@ -0,0 +1,24 @@
+const Project = require('../models/Projects.model');
+
+// Create a new project
+
+exports.createProject = async (req, res) => {
+    try {
+        const { title, description, goalAmount } = req.body;
+
+        if (!title || !description || !goalAmount)
+            return res.status(400).json({ message: "All fields are required!" });
+        
+        const adminID = req.user.id;
+        const project = await Project.create({
+            title,
+            description,
+            goalAmount,
+            createdBy: adminID
+        })
+        return res.status(201).json({message: "Project created successfully!", project });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
